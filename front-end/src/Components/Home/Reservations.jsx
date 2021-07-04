617a31998bac5d82d1a6a88beb4fb24bd65bc996
themancodes <menocorde04@gmail.com>
2021-07-04 04:36:36 -0500

    feat: chairs have unique ids/ reservations module crud

---
+++ front-end/src/Components/Home/Reservations.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react"
+import { deleteReservation, createReservation } from "../../Layouts/Home/queries"
+
+export default function Reservations({ reservations }) {
+
+  const [startTime, setStartTime] = useState("")
+  const [finishTime, setFinishTime] = useState("")
+  const [date, setDate] = useState("")
+  const [seat, setSeat] = useState("") // This is a temporary state/ dev use only
+
+
+  const user = window.localStorage.getItem("email")
+
+
+
+  const ReservationsSchedule = () => {
+    return reservations?.map((reservation, index) => {
+      return (
+        <li key={index}>
+          <span>{reservation.seat} -------</span>
+          <span>{new Date(reservation.Start).getDay()} --------</span>
+          <span>{reservation.Start.match(/\d\d:\d\d/)} --------</span>
+          <span>{reservation.Finish.match(/\d\d:\d\d/)} -------</span>
+          <button onClick={() => deleteReservation(reservation._id)} >delete</button>
+        </li>
+      )
+    })
+  }
+
+
+
+  return (
+    <div>
+      <ul>
+        <ReservationsSchedule />
+      </ul>
+      <p>Martes 8:00 am a 12pm</p>
+      <p>Miercoles 8:00 am a 12pm</p>
+      <p>Jueves 8:00 am a 12pm</p>
+      <input onChange={(event) => setSeat(event.target.value)}
+        id="seat"
+        type="text" />
+      <input onChange={(event) => setStartTime(event.target.value)}
+        id="date"
+        type="time" />
+      <input onChange={(event) => setFinishTime(event.target.value)}
+        id="start"
+        type="time" />
+      <input onChange={(event) => setDate(event.target.value)}
+        id="finish"
+        type="date" />
+      <button onClick={() => {
+        createReservation({
+          start: new Date(`${date}T${startTime}:00`),
+          finish: new Date(`${date}T${finishTime}:00`),
+          users: "60e0c85aaf2c283dd99493d2",
+          seat: seat
+        })
+      }}> + </button>
+    </div>
+  )
+  // TODO:
+  // DONE fetch reservations DONE 
+  // DONE updatestate from inputs with use effect 
+  // POST True / Done from inputs
+  
+  // toggle: on click + ( ) => display calendar input 
+}
--- front-end/src/Components/Home/Table.jsx
+++ front-end/src/Components/Home/Table.jsx
@@ -1,19 +1,21 @@
 import './Table.css';
 
-const Table = () => {
+const Table = ({ vertical, occupied, table }) => {
+
+  
+
+  function toggleChair(event){
+    console.log(event.target)
+    event.target.classList.toggle("green")
+  }
 
   return (
-  <div className="table">
-    <div className="row">
-      <div className="chair"></div>
-      <div className="chair"></div>
-    </div>
-    <div className="row">
-    <div className="tableSurface"></div>
-    </div>
-    <div className="row">
-      <div className="chair"></div>
-      <div className="chair"></div>
+    <div className={vertical ? "table-v" : "table"}>
+      <div onClick={toggleChair} id={`${table}-1`} className={occupied ? "chair-occupied" : "chair"}/>
+      <div onClick={toggleChair} id={`${table}-2`} className={occupied ? "chair-occupied" : "chair"}/>
+      <div onClick={toggleChair} className="tableSurface"/>
+      <div onClick={toggleChair} id={`${table}-3`} className={occupied ? "chair-occupied" : "chair"}/>
+      <div onClick={toggleChair} id={`${table}-4`} className={occupied ? "chair-occupied" : "chair"}/>
     </div>
 </div>
 )}
